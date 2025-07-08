94c9793f04acae1bf3ce372c8323b57295ddb0f3
Srilakshmanan <86874168+Srilakshmanan@users.noreply.github.com>
2025-07-08 19:13:04 -0400

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -1,8 +1,8 @@
 package com.mt.services;
 
-import jakarta.servlet.*;
-import jakarta.servlet.annotation.*;
-import jakarta.servlet.http.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.json.JSONException;
 import org.json.JSONObject;
